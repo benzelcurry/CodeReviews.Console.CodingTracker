e63c5489a5a5267aefddf68da18e9b09cd07e81e
Benzel <benawhiting@gmail.com>
2025-03-13 16:26:17 -0700

    Add method for ensuring database and table exist

---
+++ CodingTracker/DatabaseManager.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Configuration;
+using System.Data;
+
+namespace CodingTracker;
+
+internal class DatabaseManager
+{
+    private readonly string? _connectionString;
+
+    internal DatabaseManager()
+    {
+        _connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+    }
+
+    internal void EnsureDatabaseExists()
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+                throw new InvalidOperationException("ConnectionString is missing from the config file");
+
+            using IDbConnection connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            const string createTable = @"
+            CREATE TABLE IF NOT EXISTS coding_tracker (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                start_time TEXT NOT NULL,
+                end_time TEXT NOT NULL,
+                duration TEXT NOT NULL
+            );";
+
+            connection.Execute(createTable);
+
+            Console.WriteLine("Database and table are ready.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error establishing connection with database: {ex.Message}");
+        }
+    }
+}
--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodingTracker;
+
+DatabaseManager dbManager = new();
+dbManager.EnsureDatabaseExists();
