71cad39b12f66c6f35effd69559280a44b305add
Benzel <benawhiting@gmail.com>
2025-03-17 16:38:55 -0700

    Add model for coding sessions

---
+++ CodingTracker/Models/CodingSession.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingTracker.Models;
+
+internal class CodingSession
+{
+    internal required int Id { get; set; }
+    internal required string StartTime { get; set; }
+    internal required string EndTime { get; set; }
+    internal required string Duration { get; set; }
+}
