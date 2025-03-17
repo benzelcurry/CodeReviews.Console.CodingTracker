a96240eb8cf3df15d4ba82e57fe15f46b3c6f423
Benzel <benawhiting@gmail.com>
2025-03-17 16:45:35 -0700

    Add enum for months

---
+++ CodingTracker/Models/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingTracker.Models;
+internal class Enums
+{
+    internal enum MonthOption
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
